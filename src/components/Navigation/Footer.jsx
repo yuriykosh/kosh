808a0da59097045e9dbd5e2b108e38b439cfdb91
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2024-07-06 23:51:50 +0200

    changes

---
+++ src/components/Navigation/Footer.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <header className="bg-black ">
+      <p>footer</p>
+    </header>
+  );
+};
+
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,19 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "2xl": "1440px",
+      // => @media (min-width: 1440px) { ... }
+    },
+    colors: {
+      blue: "#2C37CD",
+      white: "#DBD5C6",
+      magenta: "#D023EA",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        erode: ["var(--font-erode)"],
+        satoshi: ["var(--font-satoshi)"],
       },
     },
   },
